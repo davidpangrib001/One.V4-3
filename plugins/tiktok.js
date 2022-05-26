c81255fe5a6545f4874891134d7c8d77783d84e4
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-05-27 06:35:09 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,57 +1,9 @@
-let moment = require('moment-timezone')
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-let fs = require('fs')
-let fetch = require('node-fetch')
-let handler = async (m, {text}) => {
-if (!text) return conn.reply(m.chat, 'Harap masukkan link\n\nContoh: .tiktok https://tiktok.com/xxxxxx', m)
+const hxz = require("hxz-api")
+let handler = async(m,{text, conn}) => {
 
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-    let user = global.db.data.users[who]
-    let more = String.fromCharCode(8206)
-    let readMore = more.repeat(4001)
-let anu = `*── 「 TIKTOK 」 ──*
-
-SILAHKAN PILIH`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: fs.readFileSync('./src/welcome.jpg')}, 
-           hydratedFooterText: wm,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '📍instagram',
-               url: instagram
-               }
-               
-             },
-             {
-             quickReplyButton: {
-               displayText: 'Tanpa Watermark',
-               id: `.tiktoknowm ${text}`,
-             }
-           },
-           {
-             quickReplyButton: {
-               displayText: 'dengan watermark',
-               id: `.tiktokwm ${text}`,
-             }
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
+let p = await  hxz.ttdownloader(text)
+const { nowm, wm, audio } = p
+conn.sendFile(m.chat, nowm, null, 'Lagi Dikasih Bilang Apa ??', m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-
+handler.command = ['tiktok']
 module.exports = handler
